976f8145d083fa46627b3ef250e01ee7a73c96a9
savahire8 <andrey.savkin@lamoda.ru>
2017-03-25 02:22:05 +0300

    Got rid of sqlite

--- frets.py
+++ frets.py
@@ -24,7 +24,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.urandom(24)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
 bootstrap = Bootstrap(app)
